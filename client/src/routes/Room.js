77c8efb5903ccd04ae8c3b06f05d04cbefa8bc26
Sap-thoorne <65505963+Sap-thoorne@users.noreply.github.com>
2020-06-02 17:46:41 +0530

    Update Room.js

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -45,7 +45,8 @@ const Room = (props) => {
     const roomID = props.match.params.roomID;
 
     useEffect(() => {
-        socketRef.current = io.connect("/");
+//         socketRef.current = io.connect("/");
+        socketRef.current = io();
         navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
             userVideo.current.srcObject = stream;
             socketRef.current.emit("join room", roomID);
